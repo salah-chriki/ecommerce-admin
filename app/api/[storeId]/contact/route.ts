94a3f1f92d2dd10313566ba1b1af07825be1e36f
Salah <salahchriki1@gmail.com>
2024-03-24 22:38:42 +0000

    contact

--- app/api/[storeId]/contact/route.ts
+++ app/api/[storeId]/contact/route.ts
@@ -19,5 +19,37 @@ export async function POST(
     return new NextResponse("Store id is required", { status: 400 });
   }
 
+  const mailjet = require("node-mailjet").connect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE
+  );
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: body.email,
+          Name: body.name,
+        },
+        To: [
+          {
+            Email: "salahchriki1@gmail.com",
+            Name: "Salah Chriki",
+          },
+        ],
+        Subject: "New Contact Form!",
+        TextPart: body.message,
+        HTMLPart:
+          '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
+      },
+    ],
+  });
+  request
+    .then((result: Request) => {
+      console.log(result.body);
+    })
+    .catch((err: Error) => {
+      console.log(err.message);
+    });
+
   return NextResponse.json(body, { status: 200, headers: corsHeaders });
 }
