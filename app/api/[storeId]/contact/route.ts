e8f39381997d7cd26d201bd5ae0c002411ac5abd
Salah <salahchriki1@gmail.com>
2024-03-24 23:41:28 +0000

    contact route

--- app/api/[storeId]/contact/route.ts
+++ app/api/[storeId]/contact/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
+import FormData from "form-data";
+import Mailgun from "mailgun.js";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -20,41 +22,24 @@ export async function POST(
     return new NextResponse("Store id is required", { status: 400 });
   }
 
-  const mailjet = require("node-mailjet").connect(
-    process.env.MJ_APIKEY_PUBLIC,
-    process.env.MJ_APIKEY_PRIVATE
-  );
-  const request = mailjet.post("send", { version: "v3.1" }).request({
-    Messages: [
-      {
-        From: {
-          Email: email,
-          Name: name,
-        },
-        To: [
-          {
-            Email: "salahchriki1@gmail.com",
-            Name: "Salah Chriki",
-          },
-        ],
-        Subject: "New Contact Form!",
-        TextPart: message,
-        HTMLPart:
-          '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
-      },
-    ],
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY || "key-yourkeyhere",
   });
 
-  request
-    .then((result: Request) => {
-      console.log(result.body);
+  mg.messages
+    .create("sandbox-123.mailgun.org", {
+      from: "Contact Form <mailgun@sandbox-123.mailgun.org>",
+      to: ["salahchriki1@gmail.com"],
+      subject: "New Contact Form!",
+      text: message,
+      html: `<h1>Hello</h1>
+  
+  <p>Name: ${name} his email is ${email}</p>   `,
     })
-    .catch((err: any) => {
-      console.log(err.statusCode);
-    });
+    .then((msg) => console.log(msg)) // logs response data
+    .catch((err) => console.log(err));
 
-  return NextResponse.json(
-    { submitted: true },
-    { status: 200, headers: corsHeaders }
-  );
+  return NextResponse.json({ submitted: true }, { headers: corsHeaders });
 }
