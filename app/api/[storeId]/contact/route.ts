9dc5d2e627c55625d203f21a74ff9f8ab5d2f524
Salah <salahchriki1@gmail.com>
2024-03-24 21:23:11 +0000

    contact

--- app/api/[storeId]/contact/route.ts
+++ app/api/[storeId]/contact/route.ts
@@ -7,7 +7,7 @@ const corsHeaders = {
 };
 
 export async function OPTIONS() {
-  return NextResponse.json({}, { status: 200, headers: corsHeaders });
+  return NextResponse.json({}, { headers: corsHeaders });
 }
 
 export async function POST(
